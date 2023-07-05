d8e5ce70cf2f52b15ba11f66e8571078642991b5
Roman Bafanov <108800547+RomanBafanov@users.noreply.github.com>
2023-07-05 10:50:05 +0300

    greated API2

--- main.py
+++ main.py
@@ -1,16 +1,75 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from typing import Any
+from urllib.parse import urlparse
+from dotenv import load_dotenv
+import os
+import requests
 
 
-# Press the green button in the gutter to run the script.
+BODY = {}
+
+URL = 'https://api-ssl.bitly.com/v4/bitlinks'
+
+
+def shows_clicks(link, headers) -> Any:
+    try:
+        clicks = count_clicks(headers, link)
+    except requests.exceptions.HTTPError:
+        return None
+    else:
+        return clicks
+
+
+def count_clicks(headers, link) -> Any:
+    url = f'{URL}/{link}/clicks/summary'
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+
+    return response.json()['total_clicks']
+
+
+def cut_and_show(link, headers):
+    BODY['long_url'] = link
+    try:
+        bitlink = shorten_link(headers, URL, BODY)
+    except requests.exceptions.HTTPError:
+        return None
+    else:
+        return bitlink
+
+
+def shorten_link(headers, url, body) -> Any:
+    response = requests.post(url, headers=headers, json=body)
+    response.raise_for_status()
+
+    return response.json()['link']
+
+
+def is_bitlink(url):
+    parsed = urlparse(url)
+    if parsed.scheme == '':
+        return False
+    else:
+        return True
+
+
+def main(headers) -> Any:
+    user_input = input('Введите ссылку ')
+    if is_bitlink(user_input):
+        bitlink = cut_and_show(user_input, headers)
+        if bitlink != None:
+            print('Битлинк', bitlink)
+        else:
+            print('Ошибка ввода!')
+    else:
+        clicks = shows_clicks(user_input, headers)
+        if clicks != None:
+            print(f'По вашей ссылке прошли: {clicks} раз(а)')
+        else:
+            print('Ошибка ввода!')
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    load_dotenv()
+    token = os.getenv('TOKEN')
+    headers = {'Authorization': token}
+    main(headers)
